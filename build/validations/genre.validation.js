db3307348439a7858826ece4b55016336c21fa33
AdnanRohmatKurniansah <atnandimas@gmail.com>
2025-11-01 21:29:33 +0700

    fix: build path

--- build/validations/genre.validation.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GenreSchema = void 0;
-const zod_1 = require("zod");
-exports.GenreSchema = zod_1.z.object({
-    nama_genre: zod_1.z.string().min(1, 'Nama Genre is required').max(80, 'Max Character 80'),
-});
-//# sourceMappingURL=genre.validation.js.map
